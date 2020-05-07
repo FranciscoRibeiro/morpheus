4e4c6964c0761ab50424a274c775060a4f91e85c
FranciscoRibeiro <kikoribeiro95@gmail.com>
2020-05-08 00:18:18 +0100

    Update bulk processing (IDEMain) so that it excludes non-compiling and no-ast-change cases

--- src/main/java/IDEMain.kt
+++ src/main/java/IDEMain.kt
@@ -1,33 +1,43 @@
 import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
 import kotlin.system.measureTimeMillis
+
+fun notIn(prog: File, list: List<String>): Boolean {
+    return !list.any { prog.absolutePath.contains(it) }
+}
 
 fun main(args: Array<String>) {
     val mutantsPath = args[0]
     val originalsPath = args[1]
-    val mutantFiles = File(mutantsPath).walk()
-            .filter { it.toString().contains(".java") }
+    val noASTChanges = Files.readAllLines(Paths.get("reports/total_no_ast_changes.txt"))
+    val noCompile = Files.readAllLines(Paths.get("reports/total_no_compile.txt"))
+    val mutantFiles = Files.walk(Paths.get(mutantsPath)).parallel()
+            //.filter<File> { it.toString().contains(".java") }
+            /*.filter { it.extension == "java" }
+            .filter { notIn(it, noCompile) && notIn(it, noASTChanges) }*/
+
+    /*val mutantFiles = File(mutantsPath).walk()
+            //.filter<File> { it.toString().contains(".java") }
+            .filter { it.extension == "java" }
+            .filter { notIn(it, noCompile) && notIn(it, noASTChanges) }*/
+
+//    var temp = mutantFiles.toList()
 
     var counter = 0
     var timeTaken = measureTimeMillis {
-        mutantFiles.toList().parallelStream().forEach {
-            var line = infer(originalsPath + it.name, it.absolutePath)
-            File("inferred_mutants.csv").appendText("$line\n")
-            counter++
-            if(counter % 100 == 0){
-                println(counter)
-            }
-        }
-        /*for (mutant in mutantFiles){
-            *//*println(originalsPath + mutant.name)
-            println(mutant.absolutePath)
-            println("---------------------------------")*//*
-            var line = infer(originalsPath + mutant.name, mutant.absolutePath)
-            File("inferred_mutants.csv").appendText("$line\n")
-            counter++
-            if(counter % 100 == 0){
-                println(counter)
-            }
-        }*/
+        mutantFiles
+                .filter { it.toString().endsWith(".java") }
+                .map { it.toFile() }
+                .filter { notIn(it, noCompile) && notIn(it, noASTChanges) }
+                .forEach {
+                    var line = infer(originalsPath + it.name, it.absolutePath)
+                    File("inferred_mutants.csv").appendText("$line\n")
+                    counter++
+                    if (counter % 100 == 0) {
+                        println(counter)
+                    }
+                }
     }
     println(timeTaken.toFloat()/1000/60)
 }
